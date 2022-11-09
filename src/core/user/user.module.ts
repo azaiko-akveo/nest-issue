bfde3b6973925f3660d45bc4a0674fa5f37d1224
artem.zaiko.ext <artem.zaiko.ext@munichgeneral.com>
2022-11-09 15:06:11 +0300

    code clean up

--- src/core/user/user.module.ts
+++ src/core/user/user.module.ts
@@ -1,11 +1,11 @@
-import { DynamicModule, forwardRef, Module } from '@nestjs/common';
+import { DynamicModule, forwardRef, Module } from "@nestjs/common";
 
-import { ProjectModule } from '../project/project.module';
+import { ProjectModule } from "../project/project.module";
 
-import { UserService } from './user.service';
+import { UserService } from "./user.service";
 
 @Module({
-  imports: [forwardRef(() => ProjectModule.forRoot())],
+  // imports: [forwardRef(() => ProjectModule.forRoot())],
 })
 export class UserModule {
   static forRoot(): DynamicModule {
